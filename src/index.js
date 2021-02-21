32412596eca48b94026d301458037e13ece14e57
Luiz Filipe da Silva de Jesus <infobase.lfsilva@terceiros.mag.com.br>
2021-02-21 16:04:10 -0300

    installing more dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import Routes from './routes';
 
 const App = () => {
 	return (
-		<View>
-			<Text> Testando </Text>
-		</View>
+		<Routes />
 	);
 };
 
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Feed from './pages/Feed';
+
+const  Routes = createAppContainer(
+	createStackNavigator({
+		Feed
+	})
+);
