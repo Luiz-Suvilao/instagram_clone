3f97c75ab3e93e8da708ae103c46f56fd3bb0f7c
Luiz Filipe da Silva de Jesus <infobase.lfsilva@terceiros.mag.com.br>
2021-02-26 22:11:49 -0300

    creating an animated image component

---
+++ src/components/LazyImage/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Animated } from 'react-native';
+
+import { Small, OriginalImage } from './styles';
+
+const OriginalImageAnimated = Animated.createAnimatedComponent(OriginalImage);
+
+const LazyImage = ({
+	smallSource,
+	source,
+	aspectRatio,
+}) => {
+	const opacity = new Animated.Value(0);
+	const [loaded, setLoaded] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoaded(true);
+		}, 1000);
+	}, []);
+
+	function handleAnimate() {
+		Animated.timing(opacity, {
+			toValue: 1,
+			duration: 500,
+			useNativeDriver: true,
+		}).start();
+	}
+
+	return (
+		<Small
+			source={ smallSource }
+			ratio={ aspectRatio }
+			resizeMode="contain"
+			blurRadius={ 2 }
+		>
+			{ loaded && <OriginalImageAnimated
+				style={ { opacity } }
+				source={ source}
+				ratio={ aspectRatio }
+				resizeMode="contain"
+				onLoadEnd={ handleAnimate }
+			/> }
+		</Small>
+	);
+}
+
+export default LazyImage;
